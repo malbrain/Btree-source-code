2564b18d9b450d5ceab583428b90ded0b7f99424
Karl Malbrain <malbrain@yahoo.com>
2017-09-22 12:03:58 -0700

    Add disk read to systime.

--- systime.c
+++ systime.c
@@ -3,25 +3,31 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <unistd.h>
 
 void main (int argc, char **argv) {
-int fd = open (argv[1], O_CREAT | O_RDWR, 0666);
-int cnt = atoi(argv[2]), i, j;
-int cell = atoi(argv[3]);
-char *buff;
+int fd = open (argv[2], O_CREAT | O_RDWR, 0666);
+int cnt = atoi(argv[3]), i, j;
+int cell = atoi(argv[4]);
+char *buff, *map;
+int sum = 0;
 
 	buff = calloc (262144, 1);
 	buff[cell] = 1;
 
 	write (fd, buff, 262144);
 
-	for (i = 0; i < cnt; i++) {
-		char *map = mmap (NULL, 262144, PROT_READ, MAP_SHARED, fd, 0);
+	for (i = 0; i < cnt; i++)
+	  switch(argv[1][0]) {
+	  case 'm':
+		map = mmap (NULL, 262144, PROT_READ, MAP_SHARED, fd, 0);
 
 		if (map == MAP_FAILED) {
 			printf("mmap failed, errno = %d\n", errno);
 			exit(1);
 		}
+
+		sum += buff[random() % 262144];
 
 		if (!i)
 		  for (j = 0; j < 262144; j++)
@@ -29,5 +35,13 @@ char *buff;
 				printf("Found idx = %d\n", j);
 
 		munmap (map, 262144);
-	}
+		continue;
+
+	  case 'd':
+		pread (fd, buff, 262144, 0);
+		sum += buff[rand() % 262144];
+		continue;
+	  }
+
+	printf("sum %d\n", sum);
 }
