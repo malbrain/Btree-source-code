4c8d0efca880604d279d1aa2fe1178fff13457c9
Karl Malbrain <malbrain@yahoo.com>
2017-09-24 10:43:21 -0700

    Add timing info to systime.c

--- systime.c
+++ systime.c
@@ -3,9 +3,70 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <unistd.h>
 #include <inttypes.h>
+
+#ifdef _WIN32
+double getCpuTime(int type)
+{
+FILETIME crtime[1];
+FILETIME xittime[1];
+FILETIME systime[1];
+FILETIME usrtime[1];
+SYSTEMTIME timeconv[1];
+double ans = 0;
+
+	memset (timeconv, 0, sizeof(SYSTEMTIME));
+
+	switch( type ) {
+	case 0:
+		GetSystemTimeAsFileTime (xittime);
+		FileTimeToSystemTime (xittime, timeconv);
+		ans = (double)timeconv->wDayOfWeek * 3600 * 24;
+		break;
+	case 1:
+		GetProcessTimes (GetCurrentProcess(), crtime, xittime, systime, usrtime);
+		FileTimeToSystemTime (usrtime, timeconv);
+		break;
+	case 2:
+		GetProcessTimes (GetCurrentProcess(), crtime, xittime, systime, usrtime);
+		FileTimeToSystemTime (systime, timeconv);
+		break;
+	}
+
+	ans += (double)timeconv->wHour * 3600;
+	ans += (double)timeconv->wMinute * 60;
+	ans += (double)timeconv->wSecond;
+	ans += (double)timeconv->wMilliseconds / 1000;
+	return ans;
+}
+#else
+#include <time.h>
+#include <unistd.h>
+#include <sys/resource.h>
 #include <sys/mman.h>
+
+double getCpuTime(int type)
+{
+struct rusage used[1];
+struct timeval tv[1];
+
+	switch( type ) {
+	case 0:
+		gettimeofday(tv, NULL);
+		return (double)tv->tv_sec + (double)tv->tv_usec / 1000000;
+
+	case 1:
+		getrusage(RUSAGE_SELF, used);
+		return (double)used->ru_utime.tv_sec + (double)used->ru_utime.tv_usec / 1000000;
+
+	case 2:
+		getrusage(RUSAGE_SELF, used);
+		return (double)used->ru_stime.tv_sec + (double)used->ru_stime.tv_usec / 1000000;
+	}
+
+	return 0;
+}
+#endif
 
 uint64_t myrandom(uint64_t modulo) {
 uint64_t ans = 0;
@@ -38,6 +99,8 @@ int cnt = atoi(argv[3]), i, j, k;
 int scale = atoi(argv[4]);
 int upd = atoi(argv[5]);
 char *buff, *map;
+double start[3];
+float elapsed;
 int sum = 0;
 
 	if (argc < 2) {
@@ -62,6 +125,10 @@ int sum = 0;
 			exit(1);
 		}
 	}
+
+	start[0] = getCpuTime(0);
+	start[1] = getCpuTime(1);
+	start[2] = getCpuTime(2);
 
 	for (i = 0; i < cnt; i++) {
 		off = myrandom(size - 262144) & ~0xfffLL;
@@ -104,6 +171,11 @@ int sum = 0;
 		}
 	}
 
-	printf("sum %d\n", sum);
+	elapsed = getCpuTime(0) - start[0];
+	fprintf(stderr, " real %dm%.3fs\n", (int)(elapsed/60), elapsed - (int)(elapsed/60)*60);
+	elapsed = getCpuTime(1) - start[1];
+	fprintf(stderr, " user %dm%.3fs\n", (int)(elapsed/60), elapsed - (int)(elapsed/60)*60);
+	elapsed = getCpuTime(2) - start[2];
+	fprintf(stderr, " sys  %dm%.3fs\n", (int)(elapsed/60), elapsed - (int)(elapsed/60)*60);
 	return 0;
 }
