2778bc316d497f84a594c8cc0072cef9b265e486
Karl Malbrain <malbrain@yahoo.com>
2017-09-24 09:48:10 -0700

    Rework systime.c to track buffered leaf updates.

--- systime.c
+++ systime.c
@@ -22,20 +22,21 @@ uint64_t ans = 0;
 	return ans % modulo;
 }
 
-char usage[] = "usage: %s type filename reps megs [mem]\n"
+char usage[] = "usage: %s type filename reps megs [upd]\n"
 	"	where type is:\n"
 	"		c - use full file memory map\n"
 	"		d - use disk read/writes for leaves\n"
 	"	reps is the number of random pages to process\n"
 	"	filename is the name of the test disk file\n"
 	"	megs is the size of the test file in megabytes\n"
-	"	mem is the limit of core used in megabytes\n\n";
+	"	upd is number of buffered leaf updates\n\n";
 	
 int main (int argc, char **argv) {
-uint64_t size = 1024LL * 1024LL, off, core = 0;
 int fd = open (argv[2], O_CREAT | O_RDWR, 0666);
+uint64_t size = 1024LL * 1024LL, off;
 int cnt = atoi(argv[3]), i, j, k;
 int scale = atoi(argv[4]);
+int upd = atoi(argv[5]);
 char *buff, *map;
 int sum = 0;
 
@@ -43,9 +44,6 @@ int sum = 0;
 		fprintf (stderr, usage, argv[0]);
 		exit(1);
 	}
-
-	if (argc > 5)
-		core = (uint64_t)atoi(argv[5]) * 1024LL * 1024LL;
 
 	off = 0;
 	size *= scale;
@@ -55,36 +53,35 @@ int sum = 0;
 	  while (off < size)
 		pwrite (fd, buff, 1024 * 1024, off), off += 1024 * 1024;
 
-	map = mmap (NULL, size, PROT_READ, MAP_SHARED, fd, 0);
+	if(argv[1][0] == 'c') {
+		map = mmap (NULL, size, PROT_READ, MAP_SHARED, fd, 0);
 
-	if (map == MAP_FAILED) {
-		printf("core mmap failed, errno = %d\n", errno);
-		exit(1);
+		if (map == MAP_FAILED) {
+			printf("mmap failed, errno = %d\n", errno);
+			exit(1);
+		}
 	}
-
-	madvise(map, core, MADV_WILLNEED);
-	madvise(map + core, size - core, MADV_DONTNEED);
 
 	for (i = 0; i < cnt; i++) {
 		off = myrandom(size - 262144) & ~0xfffLL;
 
 		// simulate in-memory operation on interior or leaf node
 
-		if (off < core) {
-		  for (j = 0; j < 32; j++)
+		if (i % upd) {
+		  for (j = 0; j < 18; j++)
 			sum += map[off + myrandom(262144)];
 
 		  continue;
 		}
 
-		// simulate leaf level on-disk operation
+		// simulate leaf level disk operation
 
 		switch(argv[1][0]) {
 		case 'c':
 			madvise(map + off, 262144, MADV_WILLNEED);
 
-			for(k = 0; k < 400; k++)
-			 for(j = 0; j < 32; j++)
+			for(k = 0; k < upd; k++)
+			 for(j = 0; j < 18; j++)
 			  sum += buff[myrandom(262144)];
 
 			madvise(map + off, 262144, MADV_DONTNEED);
@@ -98,8 +95,8 @@ int sum = 0;
 			  exit(1);
 			}
 
-			for(k = 0; k < 400; k++)
-			 for(j = 0; j < 32; j++)
+			for(k = 0; k < upd; k++)
+			 for(j = 0; j < 18; j++)
 			  sum += buff[myrandom(262144)];
 
 			continue;
