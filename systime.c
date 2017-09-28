adf7e7ccf4f45bdcf5396d20d5c47f9e1a9c09d7
Karl Malbrain <malbrain@yahoo.com>
2017-09-28 10:27:56 -0700

    Add multi-thread capability to systime.c

--- systime.c
+++ systime.c
@@ -97,21 +97,107 @@ int height = 1;
 	return height;
 }
 
-char usage[] = "usage: %s type filename reps megs [upd]\n"
+typedef struct {
+	int fd, idx, cnt, upd;
+	char *base, *map, type;
+	uint64_t size;
+} ThreadArg;
+
+#ifndef _WIN32
+void *execround (void *vals) {
+#else
+uint32_t __stdcall execround (void *vals) {
+#endif
+ThreadArg *args = vals;
+int height, i, j, k;
+char *page;
+off_t off;
+
+	page = malloc(262144);
+
+	for (i = 0; i < args->cnt; i++) {
+		off = myrandom(args->size - 262144) & ~0xfffLL;
+		height = towerHeight(262144);
+
+		// simulate operation on interior node
+	
+		if (i % args->upd) {
+	  	  for (j = 0; j < height; j++)
+			args->base[off + myrandom(262144)] += 1;
+
+	  	  continue;
+		}
+
+		// simulate leaf level disk operation
+
+		switch(args->type) {
+		case 'm':
+			madvise(args->map + off, 262144, MADV_WILLNEED);
+
+			for(k = 0; k < args->upd; k++) {
+			 height = towerHeight(262144);
+
+			 for(j = 0; j < height; j++) {
+			  uint32_t x = myrandom(262144);
+			  args->map[off + x] = args->base[off + x];
+			 }
+			}
+
+			madvise(args->map + off, 262144, MADV_DONTNEED);
+			break;
+
+		case 'd':
+			j = pread (args->fd, page, 262144, off);
+
+			if (j < 262144) {
+			  printf("pread failed, errno = %d offset = %" PRIx64 " len = %d\n", errno, off, j);
+			  exit(1);
+			}
+
+			for(k = 0; k < args->upd; k++) {
+			 height = towerHeight(262144);
+
+			 for(j = 0; j < height; j++) {
+			  uint32_t x = myrandom(262144);
+			  page[x] = args->base[off+ x];
+			 }
+			}
+
+			j = pwrite (args->fd, page, 262144, off);
+
+			if (j < 262144) {
+			  printf("pwrite failed, errno = %d offset = %" PRIx64 " len = %d\n", errno, off, j);
+			  exit(1);
+			}
+
+			continue;
+		}
+	}
+}
+
+char usage[] = "usage: %s type filename reps megs upd thrds\n"
 	"	where type is:\n"
 	"		m - use full file memory map\n"
 	"		d - use disk read/writes for leaves\n"
 	"	reps is the number of random pages to simulate\n"
 	"	filename is the name of the test disk file\n"
 	"	megs is the size of the test file in megabytes\n"
-	"	upd is number of buffered leaf updates\n\n";
+	"	upd is number of buffered leaf updates\n"
+	"	thrds is the number of threads to fire\n\n";
 	
 int main (int argc, char **argv) {
 int fd = open (argv[2], O_CREAT | O_RDWR, 0666);
 uint64_t size = 1024LL * 1024LL, off;
-int cnt = atoi(argv[3]), i, j, k;
+int cnt = atoi(argv[3]), idx, err;
 int scale = atoi(argv[4]);
 int upd = atoi(argv[5]);
+int nthrds = atoi(argv[6]);
+#ifdef unix
+pthread_t *threads;
+#else
+HANDLE *threads;
+#endif
+ThreadArg *args;
 char page[262144];
 char *base, *map;
 double start[3];
@@ -161,69 +247,47 @@ int height;
 		exit(1);
 	}
 
+#ifdef unix
+	threads = malloc (nthrds * sizeof(pthread_t));
+#else
+	threads = GlobalAlloc (GMEM_FIXED|GMEM_ZEROINIT, nthrds * sizeof(HANDLE));
+#endif
+	args = malloc (nthrds * sizeof(ThreadArg));
+
+	//	fire off threads
+
+	for( idx = 0; idx < nthrds; idx++ ) {
+		args[idx].type = argv[1][0];
+		args[idx].size = size;
+		args[idx].base = base;
+		args[idx].map = map;
+		args[idx].idx = idx;
+		args[idx].upd = upd;
+		args[idx].cnt = cnt;
+		args[idx].fd = fd;
+#ifdef unix
+		if( err = pthread_create (threads + idx, NULL, execround, args + idx) )
+			fprintf(stderr, "Error creating thread %d\n", err);
+#else
+		threads[idx] = (HANDLE)_beginthreadex(NULL, 65536, execround, args + idx, 0, NULL);
+#endif
+	}
+
+	// 	wait for termination
+
+#ifdef unix
+	for( idx = 0; idx < cnt; idx++ )
+		pthread_join (threads[idx], NULL);
+#else
+	WaitForMultipleObjects (cnt, threads, TRUE, INFINITE);
+
+	for( idx = 0; idx < cnt; idx++ )
+		CloseHandle(threads[idx]);
+
+#endif
 	start[0] = getCpuTime(0);
 	start[1] = getCpuTime(1);
 	start[2] = getCpuTime(2);
-
-	for (i = 0; i < cnt; i++) {
-		off = myrandom(size - 262144) & ~0xfffLL;
-		height = towerHeight(262144);
-
-
-		// simulate operation on interior node
-	
-		if (i % upd) {
-	  	  for (j = 0; j < height; j++)
-			base[off + myrandom(262144)] += 1;
-
-	  	  continue;
-		}
-
-		// simulate leaf level disk operation
-
-		switch(argv[1][0]) {
-		case 'm':
-			madvise(map + off, 262144, MADV_WILLNEED);
-
-			for(k = 0; k < upd; k++) {
-			 height = towerHeight(262144);
-
-			 for(j = 0; j < height; j++) {
-			  uint32_t x = myrandom(262144);
-			  map[off + x] = base[off + x];
-			 }
-			}
-
-			madvise(map + off, 262144, MADV_DONTNEED);
-			break;
-
-		case 'd':
-			j = pread (fd, page, 262144, off);
-
-			if (j < 262144) {
-			  printf("pread failed, errno = %d offset = %" PRIx64 " len = %d\n", errno, off, j);
-			  exit(1);
-			}
-
-			for(k = 0; k < upd; k++) {
-			 height = towerHeight(262144);
-
-			 for(j = 0; j < height; j++) {
-			  uint32_t x = myrandom(262144);
-			  page[x] = base[off+ x];
-			 }
-			}
-
-			j = pwrite (fd, page, 262144, off);
-
-			if (j < 262144) {
-			  printf("pwrite failed, errno = %d offset = %" PRIx64 " len = %d\n", errno, off, j);
-			  exit(1);
-			}
-
-			continue;
-		}
-	}
 
 	elapsed = getCpuTime(0) - start[0];
 	fprintf(stderr, " real %dm%.3fs\n", (int)(elapsed/60), elapsed - (int)(elapsed/60)*60);
